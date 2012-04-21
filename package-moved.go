43c0176339863e5d7138d7072b92955f5cacd23a
Gary Burd <gary@beagledreams.com>
2012-04-21 13:11:43 -0700

    Remove top-level package.

--- package-moved.go
+++
@@ -1,6 +0,0 @@
-// Go-Mongo is now in the mongo subdirectory. Update code to import github.com/garyburd/go-mongo/mongo.
-package package_moved
-
-func fail() {
-	`Package moved to mongo subdirectory. Update code to import github.com/garyburd/go-mongo/mongo`
-}
